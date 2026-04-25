13c849a7dc7915b5299a71082b3475c159350187
Nikhil Kapila <12816113+nkapila6@users.noreply.github.com>
2026-04-25 23:40:02 +0400

    feat(webserver): finished step 2

--- webserver/main.go
+++ webserver/main.go
@@ -4,11 +4,37 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
+	"os"
 )
 
+func fetchBody(hfh, file bool) string {
+	if hfh {
+		return "Welcome to the highway to hell!"
+	}
+
+	if file {
+		body, err := os.ReadFile("index.html")
+		if err != nil {
+			fmt.Println("could not load file: ", err)
+			return "bsdk file nahi mil ri"
+		}
+		return string(body)
+	}
+
+	return "saaleya pick smth to return"
+
+}
+
 func main() {
+
+	hfh := flag.Bool("h", false, "serve HtH")
+	file := flag.Bool("f", false, "serve html file")
+	// going to consider only one arg at a time
+	flag.Parse()
+
 	listener, err := net.Listen("tcp", ":80")
 	if err != nil {
 		fmt.Println("error cannot bind requested port: ", err)
@@ -21,7 +47,7 @@ func main() {
 			fmt.Println("error in listening: ", err)
 		}
 
-		body := "Welcome to the highway to hell!"
+		body := fetchBody(*hfh, *file)
 		response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Length: %d\r\nConnection: close\r\n\r\n%s", len(body), body)
 		con.Write([]byte(response))
 
