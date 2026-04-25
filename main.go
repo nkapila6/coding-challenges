489f45e05fabe6636b4a5270ed4bcd90444e3d93
Nikhil Kapila <12816113+nkapila6@users.noreply.github.com>
2026-04-25 12:35:03 +0400

    feat: refactor for stdin

--- main.go
+++ main.go
@@ -4,12 +4,36 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"unicode/utf8"
 )
 
+func printCounts(data []byte, file string, byteCount, lines, words, chars bool) {
+	if byteCount {
+		fmt.Printf("%v %v\n", len(data), file)
+	}
+
+	if lines {
+		fmt.Printf("%v %v\n", bytes.Count(data, []byte("\n")), file)
+	}
+
+	if words {
+		fmt.Printf("%v %v\n", len(bytes.Fields(data)), file)
+	}
+
+	if chars {
+		fmt.Printf("%v %v\n", utf8.RuneCount(data), file)
+	}
+
+	if !byteCount && !lines && !words {
+		// if all are false
+		fmt.Printf("%v\t%v\t%v %v\n", bytes.Count(data, []byte("\n")), len(bytes.Fields(data)), len(data), file)
+	}
+}
+
 func main() {
-	fmt.Println("Hello from ccwc!")
+	// fmt.Println("Hello from ccwc!")
 
 	// -c, -l, -w, -m
 	byteCount := flag.Bool("c", false, "count bytes")
@@ -19,37 +43,25 @@ func main() {
 
 	flag.Parse()
 	files := flag.Args()
-	if len(files) == 0 {
-		fmt.Println("error, no file detected")
-		return
-	}
 
-	for _, file := range files {
-		data, err := os.ReadFile(files[0])
+	var data []byte
+	var err error
+
+	if len(files) == 0 {
+		data, err = io.ReadAll(os.Stdin)
 		if err != nil {
 			fmt.Println("read err: ", err)
 			return
 		}
-
-		if *byteCount {
-			fmt.Printf("%v %v\n", len(data), file)
-		}
-
-		if *lines {
-			fmt.Printf("%v %v\n", bytes.Count(data, []byte("\n")), file)
-		}
-
-		if *words {
-			fmt.Printf("%v %v\n", len(bytes.Fields(data)), file)
-		}
-
-		if *chars {
-			fmt.Printf("%v %v\n", utf8.RuneCount(data), file)
-		}
-
-		if !*byteCount && !*lines && !*words {
-			// if all are false
-			fmt.Printf("%v\t%v\t%v %v\n", bytes.Count(data, []byte("\n")), len(bytes.Fields(data)), len(data), file)
+		printCounts(data, "stdin", *byteCount, *lines, *words, *chars)
+	} else {
+		for _, file := range files {
+			data, err = os.ReadFile(file)
+			if err != nil {
+				fmt.Println("read err: ", err)
+				return
+			}
+			printCounts(data, file, *byteCount, *lines, *words, *chars)
 		}
 	}
 }
